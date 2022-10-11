c35dc1e9e91a52c065930d301ccbc07f176b3507
Alexlobe <77157997+Alexlobe@users.noreply.github.com>
2022-10-11 12:19:14 +0330

    2. Stop!.py
    Your Print Line Doesn't Work?

---
+++ 2. Stop!.py
@@ -0,0 +1,7 @@
+# Okay So Know That You Know How To Print Text, wait what? it doesnt work?
+# Ah, I Forgot To tell you about : input() : Its A Very Important Part Of The Printing.
+# It Checks For Input / Any Keys That You Press. Put it at the end of your print line then run the file. This is how it should look like :
+print('Hello World!')
+input()
+# Now If You Open The File, It'll Stay There Until You Press A Key.
+# Nice Job!
