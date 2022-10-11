7834356e7e0c6ef529de4f6a48118004fd9f8e06
Alexlobe <77157997+Alexlobe@users.noreply.github.com>
2022-10-11 12:13:09 +0330

    1. Hello World!.py
    Learn How To Print
    Hello World!
    This is a very in-depth file so don't be overwhelmed!

---
+++ 1. Hello World!.py
@@ -0,0 +1,20 @@
+# Dont Copy Paste This Code, It Will Not Work And Instead Will Give You An Error
+
+# If You Have Programmed Before, You Know What The Text 'Hello World' Is. If You Don't,
+# Its Basically The First Program Anyone Writes When They Start Coding.
+# The Program Says, 'Hello World!', So Lets Get Coding!
+
+# To PRINT Text On The Screen, We Need To Call Our Printer! So We Type :
+print
+# Now This Isnt Exactly Right Since Our Printer Doesnt Know What To Print! So Do We Do This?
+print Hello World!
+# Hmmmm.... That Doesnt Seem To Work, Maybe It Doesnt Know THATS The Text To Print! We Need To Show it To Him In A Little Bundle!
+print(Hello World!)
+# What? That Doesn't Work Too? Ohhhh Yeah, Any text that's going to be printed has to be considered as a STRING! A STRING of text!
+# To Make Text Into A String, We Need To Enclose It With :
+# Single Quotes : ''
+# Or Double Quotes : ""
+# Okay Now Lets Put Our Knowledge Into This, So call out printer and make a bundle with a STRING of TEXT! This is How It Should Look Like :
+print('Hello World!')
+# Nice! You Learned How To Print Hello World!
+# Now Dont Be Overwhelmed, I Just Talked About This Very Indepth, Nice Job!
